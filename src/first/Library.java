9ec6d614a8b43b71f739bdc9765c3f5ef3b50ef2
Hristina Rusak <hrysyarusak@yandex.by>
2023-06-07 21:16:05 +0300

    Initial commit

---
+++ src/first/Library.java
@@ -0,0 +1,10 @@
+package first;
+
+public class Library {
+    public static void main(String[] args) {
+        String nameBook = "Atlant";
+        int year = 1956;
+        double cost = 20.4;
+        System.out.println(year+" \n"+nameBook+" \n"+cost);
+    }
+}
---
+++ src/first/Square.java
@@ -0,0 +1,15 @@
+package first;
+
+import java.util.Scanner;
+
+public class Square {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        System.out.println("S = "+(a*b));
+        System.out.println("P = "+((a+b)*2));
+
+
+    }
+}

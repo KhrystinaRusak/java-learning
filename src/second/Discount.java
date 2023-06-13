193fa135a2386497f8f15fb00ec065388941e51b
Hristina Rusak <hrysyarusak@yandex.by>
2023-06-13 18:57:27 +0300

    change

---
+++ src/second/Discount.java
@@ -0,0 +1,13 @@
+package second;
+
+import java.util.Scanner;
+
+public class Discount {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите сумму товара");
+        float price = sc.nextFloat();
+        String answer = (price >= 1400) ? "Итоговая цена " + price*0.93 : "Итоговая цена " + price;
+        System.out.println(answer);
+    }
+}

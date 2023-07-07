7eefdeccfda8db5fc151266eca193de025bde2cf
Hristina Rusak <hrysyarusak@yandex.by>
2023-07-07 12:50:11 +0300

    hometask8

---
+++ src/lesson8/hometask8/task2/Square.java
@@ -0,0 +1,9 @@
+package lesson8.hometask8.task2;
+
+public class Square {
+    public static void calculate() {
+        double side = 3;
+        double s = side * side;
+        System.out.println("Сторона квадрата " + side + " Площадь квадрата " + s);
+    }
+}
